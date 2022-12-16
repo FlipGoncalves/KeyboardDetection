4be682ddb1b04b8ae4476873e79649f5398d4a85
FlipGoncalves <filipeg@ua.pt>
2022-12-16 11:51:02 +0000

    changes to main

--- main.py
+++ main.py
@@ -19,11 +19,9 @@ except:
     print(f"Error opening file, exiting...")
     exit(1)
 
-def processImage(ranges, image):
-    """
-    # Applies binary threshould operation according to given ranges
-    """
 
+""" # Applies binary threshould operation according to given ranges """
+def processImage(ranges, image):
     # processing
     mins = np.array([ranges['B']['min'], ranges['G']['min'], ranges['R']['min']])
     maxs = np.array([ranges['B']['max'], ranges['G']['max'], ranges['R']['max']])
@@ -43,25 +41,37 @@ def processImage(ranges, image):
     return image_processed
 
 
+""" Main function """
 def findCentroid(img_processed):
     connectivity = 4
     centroids = None
 
     # Perform the operation
-    _, _, _, centroids = cv2.connectedComponentsWithStats(cv2.cvtColor(img_processed, cv2.COLOR_BGR2GRAY), connectivity, cv2.CV_32S)
+    nb_components, _, stats, centroids = cv2.connectedComponentsWithStats(cv2.cvtColor(img_processed, cv2.COLOR_BGR2GRAY), connectivity, cv2.CV_32S)
 
-    for center in centroids:
+    height, width = img_processed.shape[0:2]
+
+    middle_centroid = (int(width/2), int(height/2))
+    cv2.circle(img_processed, middle_centroid, 1, (0,255,0), -1)
+
+    cent_temp = sorted(centroids, key=lambda x: ((x[0]-middle_centroid[0])**2 + (x[1]-middle_centroid[1])**2)**(1/2))
+    # print(cent_temp)
+
+    for center in cent_temp[:50]:
         cv2.circle(img_processed, (int(center[0]), int(center[1])), 1, (0,0,255), -1)
-
-    # print(len(centroids))
-    # if (len(centroids) - 100) > desvio_centroids:
-    #     print("hmm")
     
-    return centroids
+    # print(centroids)
+
+    return cent_temp
 
 
 """ Main function """
 def main():
+
+    count = 0
+    centroids = []
+    k1 = 0
+    lock = False
 
     while True:
         ret, frame = capture.read()
@@ -76,17 +86,31 @@ def main():
         # binary threshold image
         img_processed = processImage(data["limits"], frame)
 
-        # noise removal
+        # if k == ord("p") or k1 == ord("p"):
+        #     k1 = k
 
+        #     if count >= 10:
+        #         # average centroids
+        #         break
 
-        # find centroids
+        #     # find  and trim centroids
+        #     centroids.append(findCentroid(img_processed))
+
+        #     count += 1
+
+        # find  and trim centroids
         centroids = findCentroid(img_processed)
-
-        # trim centroids
         
+        if k != -1:
+            lock = not lock
+        
+        if not lock:
+            # show the final image after processing, noise removal and find the centroids
+            cv2.imshow('Processed Image', img_processed)
 
-        # show the final image after processing, noise removal and find the centroids
-        cv2.imshow('Processed Image', img_processed)
+    # print(centroids)
+    # for i in centroids:
+    #     print(len(i))
 
 
 if __name__ == "__main__":
